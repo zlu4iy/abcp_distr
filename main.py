d7a4036cef28d4d56e930d7790b717e03e381220
zlu4iy <qzen@mail.ru>
2023-09-02 12:04:33 +0300

    123

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import csv
 import requests
+from loguru import logger as log
 from bs4 import BeautifulSoup
+import pandas as pd
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
@@ -13,43 +14,53 @@ def get_data(url):
     #
     # with open(file='index.html', mode='w', encoding='utf-8') as file:
     #     file.write(response.text)
+    try:
+        with open(file='index.html', encoding='utf-8') as file:
+            src = file.read()
+            soup = BeautifulSoup(src, 'lxml')
+            table = soup.find('tbody', class_='fr-table-tbody').find_all('tr')  # можно и так table = soup.find("div", {"class": "fr-table-responsive"})
+            data = []
+            for tr in table:
+                lst = []
+                name = tr.find_next('td', class_='supplierCell').text.strip()
+                site = 'https://' + tr.find_next('span').text.strip()
+                url = tr.find_next('a').get('href')
+                city = get_city(url)
+                lst.append(name)
+                lst.append(site)
+                # data.append(url)
+                lst.append(city)
+                data.append(lst)
+            make_dataframe(data)
+    except Exception as ex:
+        log.info(f'Возникла ошибка в процессе перебора поставщиков. {ex}')
 
-    with open(file='index.html', encoding='utf-8') as file:
-        src = file.read()
-        soup = BeautifulSoup(src, 'lxml')
-        table = soup.find('tbody', class_='fr-table-tbody').find_all('tr')  # можно и так table = soup.find("div", {"class": "fr-table-responsive"})
-        lst = []
-        for tr in table[0:1]:
-            name = tr.find_next('td', class_='supplierCell').text.strip()
-            site = tr.find_next('span').text.strip()
-            link = tr.find_next('a')
-            url = link.get('href')
-            city = get_city()
-            lst.append(name)
-            lst.append(site)
-            # lst.append(url)
-            lst.append(city)
-        print(lst)
-
-        with open(file='data.csv', mode='w') as file:
-            writer = csv.writer(file)
-
-            writer.writerow(
-                (
-                    lst
-                )
-            )
-
-def get_city() -> str:
-    # response = requests.get(url=url, headers=headers)
+def get_city(url) -> str:
+    response = requests.get(url=url, headers=headers)
     #
     # with open(file='city.html', mode='w', encoding='utf-8') as file:
     #     file.write(response.text)
-    with open(file='suplier.html', encoding='utf-8') as file:
-        src = file.read()
-        soup = BeautifulSoup(src, 'lxml')
+    try:
+        soup = BeautifulSoup(response.text, 'lxml')
         city = soup.find_all('div', 'fr-form-group')[1].text
         return city
+    except Exception as ex:
+        log.info(f'Возникла ошибка в процессе получения города поставщика. {ex}')
+
+def make_dataframe(data):
+    try:
+        # log.info(data)
+        df = pd.DataFrame(data, columns=['Поставщик', 'Сайт', 'Город'])
+        log.info(df)
+        save_dataframe(df)
+    except Exception as ex:
+        log.info(f'Мне не удалось создать датафрейм. {ex}')
+
+def save_dataframe(df):
+    try:
+        df.to_excel('mydata.xlsx', index=False)
+    except Exception as ex:
+        log.info(f'Мне не удалось сохранить данные. {ex}')
 
 
 def main():
